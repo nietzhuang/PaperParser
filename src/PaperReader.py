61d63c35d49dcb53625b27e12cb6c48a83e055e0
js.huang <rock19910423@gmail.com>
2024-07-07 14:44:32 +0800

    Update PaperReader - the results write to a csv file.

--- src/PaperReader.py
+++ src/PaperReader.py
@@ -3,7 +3,9 @@ import sys
 import re
 import pdb
 
+import csv
 import PyPDF2
+# import pandas as pd
 from tqdm import tqdm
 from main import ask, log
 
@@ -13,11 +15,9 @@ class PaperReader:
         self.dir_paper = dir_paper
         self.dir_path = dir_path
         self.workpath = os.getcwd()
-        # os.chdir(dir_path)
 
     def ask_keywords(self):
         keyword_string = input('Enter your keywords to parse papers (e.g. DNN, hardware, accelerator): ') 
-        # keyword_string = 'DNN, hardware' 
         keywords = re.sub(' ', '', keyword_string).split(',')
         return keywords
 
@@ -31,15 +31,27 @@ class PaperReader:
                 log(kw + ': ' + str(hits), 'blue')
         print()
 
+    def savehits(self, paper_hits):
+        fields = ['Title']
+        rows = []
+        for kw in paper_hits[0][1].keys():
+            fields.append(kw)
+        for item in range(len(paper_hits)):
+            row = [paper_hits[item][0]] + [hits for hits in paper_hits[item][1].values()]
+            rows.append(row)
+
+        with open('ReaderResult.csv', 'w') as f:
+            write = csv.writer(f)
+            write.writerow(fields)
+            write.writerows(rows)
+            f.close
+
 
     def start(self):
         keywords = self.ask_keywords()
-        # keyword_dict = dict(zip(keywords, kw_hits))
-
-        # # os.chdir(dir_path)
-        kw_hits = [0] * len(keywords)
         paper_hits = []
         for fname in tqdm(os.listdir(self.dir_path)):
+            kw_hits = [0] * len(keywords)
             reader = PyPDF2.PdfReader(os.path.join(self.dir_path, fname), strict=False)
             for page in range(len(reader.pages)):
                 word_list = reader.pages[page].extract_text().split()
@@ -50,6 +62,7 @@ class PaperReader:
             paper_hits.append([fname, dict(zip(keywords, kw_hits))])
         
         self.printhits(paper_hits)
+        self.savehits(paper_hits)
 
 
 
